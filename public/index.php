a064f7a4d87a2e5553873bad011f8643ccbd5af0
miracuthbert <miracuthbert@gmail.com>
2023-01-18 14:09:54 +0300

    Update public index.php to handle maintenance mode correctly

--- public/index.php
+++ public/index.php
@@ -11,6 +11,21 @@ define('LARAVEL_START', microtime(true));
 
 /*
 |--------------------------------------------------------------------------
+| Check If The Application Is Under Maintenance
+|--------------------------------------------------------------------------
+|
+| If the application is in maintenance / demo mode via the "down" command
+| we will load this file so that any pre-rendered content can be shown
+| instead of starting the framework, which could cause an exception.
+|
+*/
+
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+/*
+|--------------------------------------------------------------------------
 | Register The Auto Loader
 |--------------------------------------------------------------------------
 |
